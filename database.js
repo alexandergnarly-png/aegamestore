0d0f26a0b5463426f78e06a8772dd329f8003e14
narly <alexandergnarly@gmail.com>
2026-05-31 15:50:03 +0700

    Fix mobile bottom nav tap stability and update database config

--- database.js
+++ database.js
@@ -2,9 +2,14 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected DB pool error:", err);
 });
 
 module.exports = pool;
